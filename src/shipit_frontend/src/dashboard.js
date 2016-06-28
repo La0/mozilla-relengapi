4fda6859beba78a0fe30b3432d2ba37634c4ed29
Bastien Abadie <bastien@nextcairn.com>
2016-06-28 23:45:16 +0200

    Load real data from BugZilla in Shipit frontend (demo).

--- src/shipit_frontend/src/dashboard.js
+++ src/shipit_frontend/src/dashboard.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
-import { Map, List } from 'immutable';
+import { Map } from 'immutable';
 import { combineReducers } from 'redux'
 import {routes} from './layout';
-import { fork, put } from 'redux-saga/effects'
+import { race, call, fork, put } from 'redux-saga/effects'
+import { takeLatest, delay } from 'redux-saga';
+import { fetchJSON } from './common';
+
+// Recent bugs assigned to sylvestre
+const BUGZILLA_URL = 'https://bugzilla.mozilla.org/rest/bug?assigned_to=sledru@mozilla.com&limit=10&last_change_time=2016-06-01'
+const TIMEOUT = 30;
 
 const services = routes.keySeq()
   .filter(x => x !== "dashboard")
@@ -22,16 +28,26 @@ const fetchBugs = () => {
 }
 
 /* Reducers */
-const fakeBugsReducer = (state = List(), action) => {
+const bugsReducer = (state = Map(), action) => {
   switch(action.type) {
     case 'DASHBOARD.BUGS.FETCH':
-      // Add dummy bugs in state
-      return state.push({
-        id: 12345,
-        title : 'dummy bug #1',
-      }).push({
-        id: 67890,
-        title : 'dummy bug #2',
+      // Initial state
+      return state.set('loading', true);
+
+    case 'DASHBOARD.BUGS.FETCH_SUCCESS':
+      // Load bugs from payload
+      return state.merge({
+        'error' : false,
+        'loading' : false,
+        'items' : action.payload.bugs,
+      });
+
+    case 'DASHBOARD.BUGS.FETCH_FAILED':
+      // Handle error
+      return state.merge({
+        'error' : true,
+        'loading' : false,
+        'items' : [],
       });
 
     default:
@@ -40,26 +56,73 @@ const fakeBugsReducer = (state = List(), action) => {
 }
 
 export const reducers = combineReducers({
-  bugs: fakeBugsReducer, // TODO Use real bugs provider
+  'bugs' : bugsReducer,
 })
 
 /* Sagas */
 
+const fetchBugsSaga = () => {
+  return function*(){
+
+    // Fetch data with a timeout limit
+    const { response, timeout } = yield race({
+        response: call(fetchJSON, BUGZILLA_URL),
+        timeout: call(delay, TIMEOUT * 1000)
+    });
+
+    if (response) {
+      if (response instanceof Error) {
+        yield put({
+          type: 'DASHBOARD.BUGS.FETCH_FAILED',
+          payload: response
+        });
+      } else {
+        yield put({
+          type: 'DASHBOARD.BUGS.FETCH_SUCCESS',
+          payload: response
+        });
+      }
+    } else {
+      yield put({
+        type: 'DASHBOARD.BUGS.FETCH_FAILED',
+        payload: 'Timeout (' + TIMEOUT + ') reached!'
+      });
+    }
+  }
+}
+
+const watchFetchBugs = () => {
+  return function* () {
+    // On a bugs fetch signal, actually fetch !
+    yield takeLatest('DASHBOARD.BUGS.FETCH', fetchBugsSaga());
+  };
+};
+
+
 const initialFetch = () => {
   return function*(){
-    console.info('Initial fake bugs loading...');
     yield put(fetchBugs());
-  };
+  }
 }
 
 export const sagas = [
-  fork(initialFetch())
+
+  // Fetch bugs when needed
+  fork(watchFetchBugs()),
+
+  // Send signal to start fetching
+  // Must be after watcher
+  fork(initialFetch()),
 ]
 
 /* Mappings */
 const mapStateToProps = state => {
-  // Simply give full state for demo
-  return state.toJS();
+  let input = state.toObject()
+  if(!input.dashboard)
+    return {}
+  return {
+    'bugs' : input.dashboard.bugs.toJS()
+  }
 }
 
 const mapDispatchToProps = dispatch => {
@@ -69,24 +132,32 @@ const mapDispatchToProps = dispatch => {
 
 
 /* Components */
-const Bug = ({id, title}) => (
+const Bug = ({id, product, summary}) => (
   <div className="row">
-    <div className="col-xs-6">
-      BUG #{id} : {title}
+    <div className="col-xs-12">
+      <h5>{summary}</h5>
+      BUG #{id} on {product}
+      <hr />
     </div>
   </div>
 )
 Bug.__name__ = 'Bug'
 
-export const Dashboard = ({dashboard}) => (
-  <div>
+export const Dashboard = props => {
+  let {
+    bugs = {},
+  } = props;
+  return (
     <div className="container-fluid">
-      {dashboard.bugs.map(bug =>
-        <Bug key={bug.id} {...bug} />
-      )}
+      {(!bugs || bugs.loading == true) ?
+      <p className="alert alert-info">Loading...</p>
+      :
+      <div>
+        {bugs.items && bugs.items.map(bug => <Bug key={bug.id} {...bug} />)}
+      </div>}
     </div>
-  </div>
-)
+  )
+}
 Dashboard.__name__ = 'Dashboard'
 
 export default connect(
